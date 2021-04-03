82ea9009aed1a3566705569c99a251e5b90cdc78
unknown <smc5720@gmail.com>
2021-04-03 19:32:27 +0900

    20210403

--- src/App.js
+++ src/App.js
@@ -8,24 +8,53 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'read',
+      selected_content_id: 2,
       subject: { title: 'WEB', sub: 'World Wide Web!' },
+      welcome: { title: 'Welcome', desc: 'Hello, React!!' },
       contents: [
-        { id: 1, title: 'HTML', decs: 'HTML is for information.' },
-        { id: 2, title: 'CSS', decs: 'CSS is for design.' },
-        { id: 3, title: 'JavaScript', decs: 'JavaScript is for interactive.' }
+        { id: 1, title: 'HTML', desc: 'HTML is for information.' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design.' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive.' }
       ]
     }
   }
 
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+    }
     return (
       <div className="App">
         <Subject
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}>
         </Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language!!"></Content>
+        <TOC
+          onChangePage={function (id) {
+            this.setState({
+              mode: 'read',
+              selected_content_id: Number(id)
+            });
+          }.bind(this)}
+          data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
